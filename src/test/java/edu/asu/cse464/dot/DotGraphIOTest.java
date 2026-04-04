44a090d0be94248bac8e18a3a82271e1c67f1310
codingrear <eavila12@asu.edu>
2026-04-03 22:47:17 -0700

    adjusted bfs

--- src/test/java/edu/asu/cse464/dot/DotGraphIOTest.java
+++ src/test/java/edu/asu/cse464/dot/DotGraphIOTest.java
@@ -1,24 +1,24 @@
-  @Test
-  void bfs_graph_search_finds_a_path() {
-    DirectedGraph g = new DirectedGraph();
-    g.addEdge("a", "b");
-    g.addEdge("a", "c");
-    g.addEdge("b", "d");
-    g.addEdge("c", "d");
+@Test
+void bfs_graph_search_finds_a_path() {
+  DirectedGraph g = new DirectedGraph();
+  g.addEdge("a", "b");
+  g.addEdge("a", "c");
+  g.addEdge("b", "d");
+  g.addEdge("c", "d");
 
-    Path path = g.GraphSearch(g.getNode("a"), g.getNode("d"));
+  edu.asu.cse464.dot.Path path = g.GraphSearch(g.getNode("a"), g.getNode("d"));
 
-    assertNotNull(path);
-    assertEquals("a -> b -> d", path.toString());
-  }
+  assertNotNull(path);
+  assertEquals("a -> b -> d", path.toString());
+}
 
-  @Test
-  void bfs_graph_search_returns_null_when_unreachable() {
-    DirectedGraph g = new DirectedGraph();
-    g.addEdge("a", "b");
-    g.addNode("z");
+@Test
+void bfs_graph_search_returns_null_when_unreachable() {
+  DirectedGraph g = new DirectedGraph();
+  g.addEdge("a", "b");
+  g.addNode("z");
 
-    Path path = g.GraphSearch(g.getNode("a"), g.getNode("z"));
+  edu.asu.cse464.dot.Path path = g.GraphSearch(g.getNode("a"), g.getNode("z"));
 
-    assertNull(path);
-  }
+  assertNull(path);
+}
